387757bc28079c88def18a94783501421031dd36
hardikjumnani <sauhard74best@gmail.com>
2025-03-23 10:22:40 +0530

    c

--- server.js
+++
@@ -1,9 +0,0 @@
-import { approveRequest, rejectRequest } from './api/creditRequests.js';
-
-// Make sure this is at the top of your Express setup
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Make sure these routes are directly registered with the app, not a subrouter
-app.post('/api/admin/credit-requests/approve', isAdmin, approveRequest);
-app.post('/api/admin/credit-requests/reject', isAdmin, rejectRequest); 
